0a3694d4f892ee5dbe3806165941a926600bf178
SARATH K <142706927+sarathk73@users.noreply.github.com>
2024-04-08 10:38:37 +0530

    Updated server.js: Added routes for task handling, integrating task routes into the Express application for CRUD operations

--- server.js
+++ server.js
@@ -17,6 +17,9 @@ mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTop
 const authRoutes = require('./src/routes/authRoutes');
 
 
+const taskRoutes = require('./src/routes/taskRoutes');
+
+
 app.use(cors());  // Allows all cross-origin requests
 
 
@@ -24,6 +27,8 @@ app.use(express.json());
 
 
 app.use('/api/user', authRoutes);
+
+app.use('/api/tasks', taskRoutes);
 
 
 const PORT = process.env.PORT || 3001;
